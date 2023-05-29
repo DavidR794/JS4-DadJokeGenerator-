3123fd17fb3b99fe1e28a9d53997d8bede45001a
DavidR794 <105678612+DavidR794@users.noreply.github.com>
2023-05-29 14:45:33 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//Initialis
+const btnE1 = document.getElementById("btn"); //target button from index.HTML
+const ratingsE1 = document.getElementById("ratings container"); //Access the joke and show it in the container
+
+//buttons, ratings container
+
+//Function
+
+//Event listeners
