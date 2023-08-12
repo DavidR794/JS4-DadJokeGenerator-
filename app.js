e2da45f3d12b52155b124b035a9c9b99eeb278e9
DavidR794 <105678612+DavidR794@users.noreply.github.com>
2023-08-12 14:54:19 +0100

    updated the comments section

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 //IMPORT statements if any
 
 //VARIABLE DECLARATIONS...........................................
-const btnEl = document.getElementById("btn"); //target button from index.HTML
+const sendReviewBtnEl = document.getElementById("btn"); //target button from index.HTML
 const ratingsEls = document.querySelectorAll(".rating"); //Access all the classes with name of rating
 const containerEl = document.getElementById("container"); //accesses the container
 let selectedRating = ""; //selectedRating value is an empty string
 
 //FUNCTION DEFINITIONS................................................
-/*removes the 'active' from other ratings, when one is clicked 
-e.g. removes green shadow from emoji
-/It ensures that only one rating can be active at a time.*/
 function removeActive() {
   ratingsEls.forEach(function (ratingE1) {
     ratingE1.classList.remove("active");
@@ -17,8 +14,6 @@ function removeActive() {
 }
 
 //EVENT LISTENERS.......................................................
-//access the ratings x3buttons...........
-//attaches a "click" event listener to each rating element using a forEach loop.
 ratingsEls.forEach(function (ratingE1) {
   ratingE1.addEventListener("click", function (e) {
     removeActive();
@@ -27,21 +22,8 @@ ratingsEls.forEach(function (ratingE1) {
     e.target.parentNode.classList.add("active");
   });
 });
-//WHAT THE CODE ABOVE DOES:
-//removeActive() //function to remove the "active" class from all the elements(ratings).
-//selectedRating //accesses the element whether element or(||) container
-//etargetclassList //Add the "active" class, visually indicates that the rating is selected or highlighted.
-//etargetparentNode:
-/*Add the "active" class to the parent element ensures that the background color 
-of parent element (container) remains active along with the clicked rating.*/
 
-//innertext= targeting the element itself the child the one rating clickend
-//parentNode = targets the immediate container or wrapper element that holds the target element.
-
-/*clicking button to show innertext and using dynamic innerHTML
- use of template literals to show selectedRating is equal to whatever rating is clicked.
- */
-btnEl.addEventListener("click", function (e) {
+sendReviewBtnEl.addEventListener("click", function (e) {
   if (selectedRating !== "") {
     containerEl.innerHTML = `
         <strong class= "greeting1" > Thanks for taking part in the review </strong>
@@ -52,29 +34,68 @@ btnEl.addEventListener("click", function (e) {
         `;
   }
 });
+
 /*code above adds click event to "send review" button
 1st. rating(emoji) is selected then send review buton is clicked 
 This then shows the other side of the container we add an "inner.html" with a 
- greeting message.*/
+ greeting message.
 
-// Main Logic or additional code ..........................................
+ *COMMENTS
+************ VARIABLE DECLARATIONS *******************
+1. selectedRating
+selectedRating is initialized as an empty string ("") 
+so that it has a value to start with, even before a user selects a rating.
+common practice in programming, especially when you want to 
+store a value that will be updated based on user interactions.
 
-// Export statements (if applicable)..........................................
 
-/*parents is the container:
-    selectedRating = e.target.innerText || e.target.parentNode.innerText;
-    e.targer.innertext = checks to see if in element/rating has text content inside it.
-    OR
-    e.target.parentNode.innerText = it checks the parent which is the box
-    around the outside if it has innertext
+ ************ FUNCTION DEFINITIONS *******************
+1.remove Active
+removes the 'active' from other ratings, when one is clicked 
+e.g. removes green shadow from emoji
+It ensures that only one rating can be active at a time.
 
+
+
+************ EVENT LISTENERS *******************
+1.ratingE1.addEventListener
+Allows access to all 3 ratings emojis ,attaches a "click" event listener to each 
+rating element using forEach loop.
+removeActive = this function removes the "active" class from all the emoji ratings elements
+
+
+selectedRating = used to keep track of the rating emoji that is currently selected.
+e.target.innerText || e.target.parentNode.innerText = 
+The selectedRating can either be 2 options
+The innerText =meaning it gets the text inside that emoji rating or 
+parentNode = meaning it gets the text from the container around that emoji.
+
+classlist means = accessing the classes of that element. 
+e.g. classlist of emoji element in e.targetclassList.
+
+etargetclassList = visually adds the "active" class, indicating that the emoji rating is selected or highlighted.
+etargetparentNode= visually adds the "active" class to the parent element ensures that 
+the background color of parent element (container) remains active along with the selectedRating.
+
+2.sendReviewBtnEl
+selectedRating !== ""
+If a user hasn't selected any emoji rating 
+(meaning the selectedRating is still an empty string), 
+the content won't change when the button is clicked. 
+If a rating has been selected (meaning selectedRating is something 
+other than an empty string), 
+the content on the page will change according to the chosen rating.
+Template literals to show selectedRating is equal to whatever rating is clicked.
+
+
+
+active - is like hover effect , it makes sure when rating is clicked 
+the hover stays and does not disappear when you click away
+
+ parents is the container:
 child is the emoji inside the divbox
  1.Line was removed from ratingsEls. function:
     console.log(e.target.innerText || e.target.parentNode.innerText);
 console.log(e.target.innerText); - shows ratings in console when CLICKED OUTSIDE the box, happy,angry satisfied
-e.target.parentNode.innerText - click on emoji will show rating text in console.
-2. active - is like hover effect , it makes sure when rating is clicked the hover stays and does not disappear when you click away
-3. e.target.classList.add("active") - adds active to classlist in html
-4. e.target.parentNode.classList.add("active")- makes background stay once emoji is clicked
 
 */
